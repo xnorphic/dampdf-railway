1660f78d21fdd25a55bff05a105f23ca4e742785
xnorphic <xnorphic@gmail.com>
2025-06-01 01:18:39 +0530

    Update api.py
    v2

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -1,8 +1,10 @@
+# File: app/api/api_v1/api.py
+
 from fastapi import APIRouter
-from app.api.api_v1.endpoints import files, processing, download
+from app.api.api_v1.endpoints import files, process, download, pricing
 
 api_router = APIRouter()
-
-api_router.include_router(files.router, prefix="/files", tags=["files"])
-api_router.include_router(processing.router, prefix="/process", tags=["processing"])
-api_router.include_router(download.router, prefix="/download", tags=["download"])
+api_router.include_router(files.router, prefix="/files", tags=["Files"])
+api_router.include_router(process.router, prefix="/process", tags=["Processing"])
+api_router.include_router(download.router, prefix="/download", tags=["Download"])
+api_router.include_router(pricing.router, prefix="/pricing", tags=["Pricing"])
